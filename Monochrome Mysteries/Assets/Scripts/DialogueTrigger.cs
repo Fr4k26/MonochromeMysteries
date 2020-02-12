fa5ed760adbe385be38b7520ae3c4864ebc370fb
Fr4k26 <afrak@mail.bradley.edu>
2020-02-11 21:22:05 -0600

    Dialolgue Implemented!

--- Monochrome Mysteries/Assets/Scripts/DialogueTrigger.cs
+++ Monochrome Mysteries/Assets/Scripts/DialogueTrigger.cs
@@ -8,6 +8,6 @@ public class DialogueTrigger : MonoBehaviour
 
     public void TriggerDialogue()
     {
-        FindObjectOfType<DialgoueManager>().StartDialogue(dialogue);
+      FindObjectOfType<DialgoueManager>().StartDialogue(dialogue);
     }
 }
---
+++ Monochrome Mysteries/Assets/Scripts/Trigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+class Trigger : MonoBehaviour
+{
+    public Canvas diaCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider)
+    {
+        diaCanvas.gameObject.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        diaCanvas.gameObject.SetActive(false);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
