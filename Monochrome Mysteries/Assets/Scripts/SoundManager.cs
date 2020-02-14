cf42f5d3b53d10ee3919681f290dc036cb295cb2
Jhyland-r <jakehyland001@gmail.com>
2020-02-14 06:56:10 -0600

    Duplicated Verticle Slice for Sound Testing
    I've made my own version of the current Vertical Slice scene so I can mess with it without interfering with other people. As we get closer to each milestone goal, I will start moving stuff over to whatever scene we intend to put in the build.

---
+++ Monochrome Mysteries/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    Audiosource MusicManager;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicManager = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
