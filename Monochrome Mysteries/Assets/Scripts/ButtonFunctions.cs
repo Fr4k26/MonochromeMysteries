8613a09ef54a440d17b1f9ecc87b4618d6c5faa3
Grant WH <gmwinter3111@gmail.com>
2020-02-21 12:44:56 -0600

    Implemented button for menu screens

---
+++ Monochrome Mysteries/Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void openGameScreen()
+    {
+        SceneManager.LoadScene("Verticle Slice");
+    }
+
+    public void openMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void openHowToPlay()
+    {
+        SceneManager.LoadScene("How to Play Scene");
+    }
+
+    public void openCredits()
+    {
+        SceneManager.LoadScene("Credits Scene");
+    }
+
+    public void endGame()
+    {
+        Application.Quit();
+    }
+}
