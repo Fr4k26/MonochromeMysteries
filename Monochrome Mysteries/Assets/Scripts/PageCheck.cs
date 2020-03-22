e7ff7c6c2a33886b3c6e53f2ab8f0278a60ab56c
Jhyland-r <jakehyland001@gmail.com>
2020-03-22 16:58:48 -0500

    How To Play Updated
    While the text elements aren't really changed, I made a basic system for switching pages, and that should help break things up.

---
+++ Monochrome Mysteries/Assets/Scripts/PageCheck.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageCheck : MonoBehaviour
+{
+
+    public int pageValue;
+    public int displayValue;
+    public GameObject pagePanel;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //pagePanel = this.gameObject;
+    }
+
+    public void NextPage()
+    {
+        pageValue ++;
+
+        if (pageValue > 4)
+        {
+            pageValue = 4;
+        }
+
+        if (pageValue == displayValue)
+        {
+            pagePanel.SetActive(true);
+        }
+        else if (pageValue != displayValue)
+        {
+            pagePanel.SetActive(false);
+        }
+    }
+
+    public void PreviousPage()
+    {
+        pageValue --;
+
+        if (pageValue < 1)
+        {
+            pageValue = 1;
+        }
+
+        if (pageValue == displayValue)
+        {
+            pagePanel.SetActive(true);
+        }
+        else if (pageValue != displayValue)
+        {
+            pagePanel.SetActive(false);
+        }
+    }
+}
