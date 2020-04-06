6f05045a4cc6c050fb02391387e0b70839020304
Jhyland-r <jakehyland001@gmail.com>
2020-04-06 16:42:26 -0500

    Re-implement of last push

---
+++ Monochrome Mysteries/Assets/Scripts/DoorControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorControl : MonoBehaviour
+{
+
+    public GameObject theDoor; //the specific door that is controlled by this script
+    public bool doorOpen = false; //this bool will simply allow the player to walk through the wall the represents the door
+    public AudioSource doorNoise;
+    public AudioClip open;
+    public AudioClip close;
+    public AudioClip locked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && doorOpen)
+        {
+            doorNoise.PlayOneShot(open);
+            theDoor.SetActive(false);
+        }
+        else if (other.tag == "Player" && !doorOpen)
+        {
+            doorNoise.PlayOneShot(locked);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            doorNoise.PlayOneShot(close);
+            theDoor.SetActive(true);
+        }
+    }
+}
