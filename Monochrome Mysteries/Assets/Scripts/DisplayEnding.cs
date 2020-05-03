62ce1d3d8b487fee07b0e6c7a27708d297903513
David Suriano <dsuriano@mail.bradley.edu>
2020-05-03 16:15:51 -0500

    Added All Endings

---
+++ Monochrome Mysteries/Assets/Scripts/DisplayEnding.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayEnding : MonoBehaviour
+{
+     private string winText;
+    void Start()
+    {
+        winText = PlayerPrefs.GetString("endingText");
+        gameObject.GetComponent<Text>().text = winText;
+    }
+}
