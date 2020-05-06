39b8b3dccc2a646eb736fcae6d437ae7e6229fd1
Jhyland-r <jakehyland001@gmail.com>
2020-05-05 22:43:08 -0500

    Kill Planes pt. 1

---
+++ Monochrome Mysteries/Assets/Scripts/KillPlanes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlanes : MonoBehaviour
+{
+
+    private Transform respawnPoint;
+    private Transform playerTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        respawnPoint = GameObject.Find("RespawnPoint").GetComponent<Transform>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("collide");
+        if (GetComponent<Collider>().tag == "Player")
+        {
+            playerTransform.transform.position = respawnPoint.transform.position;
+        }
+    }
+}
