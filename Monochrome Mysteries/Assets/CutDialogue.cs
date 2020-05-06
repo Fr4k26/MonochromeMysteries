1749151112fe43915846638bb14d44d814937523
David Suriano <dsuriano@mail.bradley.edu>
2020-05-06 14:36:14 -0500

    Added Cut Dialogue

---
+++ Monochrome Mysteries/Assets/CutDialogue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutDialogue : MonoBehaviour
+{
+    DialgoueManager diagMan;
+    PlayerController playerController;
+
+
+    void Start()
+    {
+        diagMan = FindObjectOfType<DialgoueManager>();
+        playerController = FindObjectOfType<PlayerController>();
+    }
+
+    void DialogueCut()
+    {
+        diagMan.paperTalking.SetBool("isTalking", false);
+        diagMan.femTalking.SetBool("isTalking", false);
+        diagMan.mobTalking.SetBool("isTalking", false);
+        diagMan.manTalking.SetBool("isTalking", false);
+
+        playerController.canmove = true;
+    }
+}
